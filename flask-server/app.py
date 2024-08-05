ac41d0b335f1b96a38f82334dfb95240e32bbbc8
LegendaryJoseph <125080328+LegendaryJoseph@users.noreply.github.com>
2024-08-05 20:20:21 +0300

    update app

--- flask-server/app.py
+++ flask-server/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Members API Route
-@app.route("/members")
+@app.route("./members")
 def members():
     return {"members": ["member1", "member2", "member3"]}
 
